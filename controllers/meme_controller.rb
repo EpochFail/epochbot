4e19baac955be2f6c5c0de4009cf162f8b732324
Chris Thorn <thorncp@gmail.com>
2014-12-11 15:27:28 -0900

    Remove !meme
    * it's broken
    * no one uses it

--- controllers/meme_controller.rb
+++ controllers/meme_controller.rb
@@ -1,9 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require "meme"
-require "tinyurl"
-
 class MemeController < Rubot::Controller
   command :awyeah do
     reply "http://bit.ly/gJJ2sb"
@@ -15,19 +12,5 @@ class MemeController < Rubot::Controller
 
   command :idunnolol do
     reply "¯\\(°_o)/¯"
-  end
-
-  command :meme do
-    begin
-      if message.text =~ /--list/
-        reply Meme::GENERATORS.map(&:first).join(", ")
-      else
-        args = Shellwords.shellsplit(message.text)
-        meme = Meme.new(args.shift)
-        reply Tinyurl.new(meme.generate(*args)).tiny
-      end
-    rescue Exception => e
-      reply e.message
-    end
   end
 end
